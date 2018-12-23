a1c0048ed332c4c8bc3036af3d6e74eac60a9ffa
Meg4R0M <psykoterro@gmail.com>
2018-12-23 19:11:06 +0100

    Base for tutorial

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+require("../style.css");
+
+export default class App extends Component {
+  render() {
+    return (
+        "Ceci est une app créée avec create-react-app"
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import App from "./components/app";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+ReactDOM.render(
+    <Provider
+        store={createStoreWithMiddleware(
+            reducers,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+      <App />
+    </Provider>,
+    document.querySelector("#root")
+);
+registerServiceWorker();
