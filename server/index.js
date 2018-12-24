0fdce3e42a46ef442073f1693ca6b8d36e6da150
Meg4R0M <psykoterro@gmail.com>
2018-12-24 23:22:29 +0100

    Preparation pour mongoDB avec mongoose et MLab

--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const expressServer = express();
 const router = require("./route");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://Meg4R0M:password123@ds145923.mlab.com:45923/dbmeg4r0m",
+    { useNewUrlParser: true }
+);
+mongoose.connection
+    .once('open', () => console.log("Connecté à MLab"))
+    .on('error', error => console.log("Erreur de connexion à MLab : ", error));
 
 // App Setup
 expressServer.use(morgan('combined'));
--- server/route.js
+++ server/route.js
@@ -1,5 +1,8 @@
+const authentificationController = require("./controllers/authentification");
+
 module.exports = function (expressServer) {
   expressServer.get("/", function (req, res, next) {
     res.send({ serverData: ["Stratocaster", "Gibson", "Ibanez"] })
   });
+  expressServer.post("/signup", authentificationController.signup);
 }
